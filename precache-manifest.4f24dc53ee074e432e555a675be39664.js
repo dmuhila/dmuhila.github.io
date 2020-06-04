b5e9ae264caba289a5978ad8c9bf9a4bf868066d
Muhila D <925dmuhila@gmail.com>
2020-06-04 01:38:54 +0100

    first commit

---
+++ precache-manifest.4f24dc53ee074e432e555a675be39664.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2e52c412dc63f592d68d79ee800e4c2",
+    "url": "/static/media/gal.e2e52c41.jpg"
+  },
+  {
+    "revision": "9779e373346a3842fc740d1b8eb189c3",
+    "url": "/static/media/DP.9779e373.jpeg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "895211aadd2d7557c597",
+    "url": "/static/js/main.6ba3f2da.chunk.js"
+  },
+  {
+    "revision": "b70e9b6428cd88a94850",
+    "url": "/static/js/2.8b5bf846.chunk.js"
+  },
+  {
+    "revision": "895211aadd2d7557c597",
+    "url": "/static/css/main.d2bc9b18.chunk.css"
+  },
+  {
+    "revision": "b105ba350afd2a2c9cfea14ceba8721a",
+    "url": "/index.html"
+  }
+];
