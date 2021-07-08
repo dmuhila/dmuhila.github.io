6375223ff36d1e5a55c0628414367ababd2fa052
dmuhila <925dmuhila@gmail.com>
2021-07-09 00:46:44 +0100

    profile update

---
+++ precache-manifest.855ef2a656cdf8b8f955859c5e038888.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "54864f8882dfc17326c4",
+    "url": "/static/css/main.87b6c824.chunk.css"
+  },
+  {
+    "revision": "54864f8882dfc17326c4",
+    "url": "/static/js/main.581993ee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "31a8ee67c3bd31aa5863",
+    "url": "/static/js/2.2070b066.chunk.js"
+  },
+  {
+    "revision": "e2e52c412dc63f592d68d79ee800e4c2",
+    "url": "/static/media/gal.e2e52c41.jpg"
+  },
+  {
+    "revision": "9779e373346a3842fc740d1b8eb189c3",
+    "url": "/static/media/DP.9779e373.jpeg"
+  },
+  {
+    "revision": "01dd775b4e6c81e2b896a6762dda21db",
+    "url": "/index.html"
+  }
+];
